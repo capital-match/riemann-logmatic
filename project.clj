062faf66c3278c73b2f5986eaf67d0958aae777f
Arnaud Bailly <arnaud.oqube@gmail.com>
2015-12-01 22:36:55 +0100

    minore change to trigger travis

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject riemann-logmatic "0.1.0-SNAPSHOT"
   :description "a riemann plugin for logmatic"
-  :url "https://github.com/capital-match/riemann-logmatic"
+  :url         "https://github.com/capital-match/riemann-logmatic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
